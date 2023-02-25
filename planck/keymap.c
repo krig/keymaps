78d138e2ce859a85e498b402ec41d30129ba41e3
Kristoffer Grönlund <krig@koru.se>
2023-02-25 10:18:46 +0100

    remove extra pgup/pgdown from lower, and add underscore to lower j

--- planck/keymap.c
+++ planck/keymap.c
@@ -58,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */ 
 
   [_LOWER] = LAYOUT_planck_grid(
-       KC_ESC,           KC_1,            KC_2,       KC_3,    KC_4,       KC_5,     KC_6,    KC_7,       KC_8,        KC_9,       KC_0, KC_PGUP,
-      _______,        _______,         _______,    _______, _______,    _______,  _______, _______, RALT(KC_P),  RALT(KC_Q), RALT(KC_W), KC_PGDN,
+       KC_ESC,           KC_1,            KC_2,       KC_3,    KC_4,       KC_5,     KC_6,    KC_7,       KC_8,        KC_9,       KC_0, _______,
+      _______,        _______,         _______,    _______, _______,    _______,  _______, KC_UNDS, RALT(KC_P),  RALT(KC_Q), RALT(KC_W), _______,
       _______,        _______,         _______,    _______, _______,    _______,  _______, _______,    _______,     _______,    KC_BSLS,  KC_GRV,
       _______,        _______,         _______,    _______, _______,    _______,    KC_NO, _______,    KC_HOME,     KC_PGDN,    KC_PGUP,  KC_END
   ),
