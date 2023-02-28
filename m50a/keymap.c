ad5b27cbddaf5da3fcf7c234f3b7a4bb96f052a7
Kristoffer Grönlund <krig@koru.se>
2023-02-28 16:53:42 +0100

    rewritten rgb light handling for m50a

--- m50a/keymap.c
+++ m50a/keymap.c
@@ -21,7 +21,8 @@
 
 
 enum custom_keycodes {
-    DBLCOLN = SAFE_RANGE // ::
+    DBLCOLN = SAFE_RANGE, // ::
+    HEXPFIX, // 0x
 };
 
 
@@ -39,14 +40,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
         LCTL(KC_UP),           KC_TAB,      KC_Q,             KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        LCTL(KC_LEFT), LCTL_T(KC_ESC),  KC_A,             KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        LCTL(KC_RGHT),        KC_LSFT,  KC_Z,             KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_QUOTE) ,
+        LCTL(KC_RGHT), LCTL_T(KC_ESC),  KC_A,             KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        LCTL(KC_LEFT),        KC_LSFT,  KC_Z,             KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_QUOTE) ,
         TG(_GAME),       MO(_NUMERIC),  LCTL(KC_LSFT), KC_LALT, KC_LGUI,   LOWER,  KC_SPC,            RAISE,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 
 
     [_LOWER] = LAYOUT(
-        _______,   KC_ESC,           KC_1,            KC_2,       KC_3,    KC_4,       KC_5,     KC_6,    KC_7,       KC_8,        KC_9,       KC_0, _______,
+        _______,   KC_ESC,           KC_1,            KC_2,       KC_3,    KC_4,       KC_5,     KC_6,    KC_7,       KC_8,        KC_9,       KC_0, HEXPFIX,
         _______,  _______,        _______,         _______,    _______, _______,    _______,  _______, KC_UNDS, RALT(KC_P),  RALT(KC_Q), RALT(KC_W), _______,
         _______,  _______,        _______,         _______,    _______, _______,    _______,  _______, _______,    _______,     _______,    KC_BSLS,  KC_GRV,
         _______,  _______,        _______,         _______,    _______, _______,    _______,           _______,    KC_HOME,     KC_PGDN,    KC_PGUP,  KC_END
@@ -76,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_1, KC_2, KC_3,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_4, KC_5, KC_6,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_7, KC_8, KC_9,
-        _______, _______, _______, _______, _______, _______, _______,          _______, _______, KC_DOT, KC_0, KC_COMMA
+        _______, _______, _______, _______, _______, _______, _______,          _______, HEXPFIX, KC_DOT, KC_0, KC_COMMA
     )
 
 };
@@ -88,6 +89,107 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
                 SEND_STRING("::");
             }
             return false;
+        case HEXPFIX:
+            if (record->event.pressed) {
+                SEND_STRING("0x");
+            }
+            return false;
     }
     return true;
+}
+
+
+void backlight_set_color( int index, uint8_t red, uint8_t green, uint8_t blue );
+void backlight_set_color_all( uint8_t red, uint8_t green, uint8_t blue );
+
+#define LC_FN1 15
+#define LC_FN2 16
+#define LC_FN3 (36+15)
+#define LC_FN4 (36+7)
+
+#define LC_W 12
+#define LC_A 5
+#define LC_S 4
+#define LC_D 3
+
+#define LC_NUM (36+6)
+#define LC_NUMP1 (18+4)
+#define LC_NUMP2 (18+5)
+#define LC_NUMP3 (18+6)
+#define LC_NUMP4 (18+12)
+#define LC_NUMP5 (18+13)
+#define LC_NUMP6 (18+14)
+#define LC_NUMP7 (54+4)
+#define LC_NUMP8 (54+5)
+#define LC_NUMP9 (54+6)
+#define LC_NUMP0 (54+13)
+
+#define LC_LOWER (36+2)
+#define LC_RAISE (54+10)
+#define LC_SPACE (36+1)
+
+static uint8_t modcolor(uint8_t c, float amt) {
+    float cf = c * amt;
+    if (cf < 0) cf = 0;
+    if (cf > 255.f) cf = 255.f;
+    return (uint8_t)cf;
+}
+
+// Override this to replace backlight
+// settings
+void backlight_effect_indicators(void) {
+    backlight_set_color_all(0, 0, 0);
+    uint8_t r = 0xfb, g = 0x85, b = 0x00;
+    backlight_set_color(LC_FN4, r/2, g/2, b/2);
+    
+    if (IS_LAYER_ON(_GAME)) {
+        r = 255; g = 0; b = 0;
+        backlight_set_color(LC_W, r, g, b);
+        backlight_set_color(LC_A, r, g, b);
+        backlight_set_color(LC_S, r, g, b);
+        backlight_set_color(LC_D, r, g, b);
+        backlight_set_color(LC_FN4, r, g, b);
+    } else if (IS_LAYER_ON(_NUMERIC)) {
+        r = 0x01; g = 0x97; b = 0xf6;
+        backlight_set_color(LC_NUM, r, g, b);
+        
+        backlight_set_color(LC_NUMP1, r, g, b);
+        backlight_set_color(LC_NUMP2, r, g, b);
+        backlight_set_color(LC_NUMP3, r, g, b);
+
+        r = modcolor(r, 0.6);
+        g = modcolor(g, 0.6);
+        b = modcolor(b, 0.6);
+        backlight_set_color(LC_NUMP4, r, g, b);
+        backlight_set_color(LC_NUMP5, r, g, b);
+        backlight_set_color(LC_NUMP6, r, g, b);
+        
+        r = modcolor(r, 0.6);
+        g = modcolor(g, 0.6);
+        b = modcolor(b, 0.6);
+        backlight_set_color(LC_NUMP7, r, g, b);
+        backlight_set_color(LC_NUMP8, r, g, b);
+        backlight_set_color(LC_NUMP9, r, g, b);
+        
+        r = modcolor(r, 0.6);
+        g = modcolor(g, 0.6);
+        b = modcolor(b, 0.6);
+        backlight_set_color(LC_NUMP0, r, g, b);
+
+    } else if (IS_LAYER_ON(_ADJUST)) {
+        backlight_set_color(LC_FN1, r, g, b);
+        r = 0x02; g = 0x30; b = 0x47; 
+        backlight_set_color(LC_LOWER, r, g, b);
+        backlight_set_color(LC_RAISE, r, g, b);
+        backlight_set_color(LC_SPACE, 255, 0, 0);
+    } else if (IS_LAYER_ON(_RAISE)) {
+        r = 0x02; g = 0x30; b = 0x47; 
+        backlight_set_color(LC_RAISE, r, g, b);
+        backlight_set_color(LC_SPACE, 0, 0, 255);
+    } else if (IS_LAYER_ON(_LOWER)) {
+        r = 0x02; g = 0x30; b = 0x47; 
+        backlight_set_color(LC_LOWER, r, g, b);
+        backlight_set_color(LC_SPACE, 0, 255, 0);
+    } else {
+    }
 }
